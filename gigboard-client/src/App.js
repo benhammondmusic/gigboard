4e62bddc5ff1a25c7c777c649cc7e886cf17ccc5
JC Coles <jc@JCs-MacBook-Pro-2.local>
2021-03-13 14:25:49 -0800

    gig list page and route added

--- gigboard-client/src/App.js
+++ gigboard-client/src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 
 import Routes from './config/routes'
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar/>
+
     <main>
       <Routes />
     </main>
+    </>
   );
 }
 
--- gigboard-client/src/config/routes.js
+++ gigboard-client/src/config/routes.js
@@ -1,10 +1,12 @@
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home/Home'
+import GigList from '../pages/GigList/GigList'
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/gigs" component={GigList} />
     </Switch>
 );
 
