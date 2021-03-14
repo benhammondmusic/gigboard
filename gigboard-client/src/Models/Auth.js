e3dcf6361bbff1e2484e321d4bbdc37cb8aee2a2
JC Coles <jc@JCs-MacBook-Pro-2.local>
2021-03-13 20:50:50 -0800

    fixed Auth.js path

--- gigboard-client/src/Models/Auth.js
+++ gigboard-client/src/Models/Auth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class Auth {
   
   static register = ( userData ) => {
-    return axios.post('http://localhost:3001/api/v1/auth/register', userData );
+    return axios.post('http://localhost:3001/register', userData );
   }
 
 }
