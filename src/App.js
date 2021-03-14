0dad4c1dfe9aa5156d18da1a55ad8f393ff57ae6
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-14 00:47:20 -0700

    uses dynamic ports for dev and prod

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 // import React from 'react'
-import axios from 'axios'
+import axios from 'axios';
 import './App.css';
 // import {Button} from 'react-bootstrap/Button'
 
-import Routes from './config/routes'
+import Routes from './config/routes';
 import Navbar from './components/Navbar/Navbar';
 
-function App() {
+// DEVELOPMENT API: localhost:3001
+// (make sure it's running with nodemon, and that you have a .env file that includes
+// NODE_ENV='development'
+//
+// PRODUCTION API: use ben's deployed backend on heroku
+let API_URL;
+if (process.env.NODE_ENV === 'development') {
+  API_URL = 'http://localhost:5000';
+} else if (process.env.NODE_ENV === 'production') {
+  API_URL = 'https://ben-gigboard.herokuapp.com';
+}
 
-  
-  axios.get( 'http://localhost:3001/helloworld')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-  
+function App() {
+  axios
+    .get(`${API_URL}/helloworld`)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      console.log(API_URL);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+
   return (
     <>
-    <Navbar/>
-
-    <main>
-      <Routes />
-    </main>
+      <Navbar />
+      <main>
+        <Routes />
+      </main>
     </>
   );
 }
