06b8d406bd56cbabad97d38137084542d52bf2b1
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-16 21:35:52 -0600

    app structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 // import React from 'react'
-import axios from "axios";
-import "./App.scss";
+import axios from 'axios';
+import './App.scss';
 // import {Button} from 'react-bootstrap/Button'
 
-import Routes from "./config/routes";
-import Navbar from "./components/Navbar/Navbar";
+import Routes from './config/routes';
+import Navbar from './components/Navbar/Navbar';
 
 // DEVELOPMENT API: localhost:5000 for heroku local web
 // PRODUCTION API: use ben's deployed backend on heroku
 let API_URL;
-if (process.env.NODE_ENV === "development") {
-  console.log("dev on 5000");
-  API_URL = "http://localhost:5000";
-} else if (process.env.NODE_ENV === "production") {
-  console.log("prod on kayes backend heroku");
-  API_URL = "https://kaye-gigboard.herokuapp.com";
+if (process.env.NODE_ENV === 'development') {
+  console.log('dev on 5000');
+  API_URL = 'http://localhost:5000';
+} else if (process.env.NODE_ENV === 'production') {
+  console.log('prod on kayes backend heroku');
+  API_URL = 'https://kaye-gigboard.herokuapp.com';
 }
 
 function App() {
@@ -33,13 +33,33 @@ function App() {
       // always executed
     });
 
+  /* 
+<APP> SCAFFOLDING
+<App>
+    <Nav />
+    <Routes />  // resolves to <Home /> <GigList} /> <Login /> or <Register /> based on URL
+
+</App>
+*/
+
+  /* 
+<HOME> SCAFFOLDING
+<Home>
+
+
+
+</ Home>
+
+
+*/
+
   return (
-    <>
+    <App>
       <Navbar />
       <main>
         <Routes />
       </main>
-    </>
+    </App>
   );
 }
 
