4589b3ddc5e89d984e3942baa361227619697802
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-17 13:20:21 -0600

    adds search elements and css to hide labels in accesible way

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,3 +1,11 @@
-const SearchBar = () => <>Search Bar</>;
+const SearchBar = () => (
+  <form action="/" method="get">
+    <label htmlFor="header-search">
+      <span className="visually-hidden">Search Gigs</span>
+    </label>
+    <input type="text" id="header-search" placeholder="Search Gigs" name="s" />
+    <button type="submit">Search</button>
+  </form>
+);
 
 export default SearchBar;
