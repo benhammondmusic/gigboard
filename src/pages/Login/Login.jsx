924d0b5b1d2b102399ad0072d80f258af328db8c
Kaye Roberts <pkaye@Kayes-MacBook-Pro.local>
2021-03-16 21:31:21 -0700

    Styled login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './Login.css'
 
 // import Auth from '../../Models/Auth'
 
@@ -7,22 +8,21 @@ const Login = () => {
     
     return (
         <>
-        <div>Login</div>
         <Form>
             <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
+                <Form.Label class="form-title">Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
+                <Form.Text className="text">
                 We'll never share your email with anyone else.
                 </Form.Text>
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
+                <Form.Label class='form-title'>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
             </Form.Group>
             
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" id='login-btn'>
                 Submit
             </Button>
         </Form>
