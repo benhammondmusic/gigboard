5348fccbeb11adf0045c1b95c4404621440b21d9
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-17 23:09:41 -0600

    connected frontend /newgig to <GigAdder>

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,17 +1,19 @@
 import { Switch, Route } from 'react-router-dom';
 
-import Home from '../pages/Home/Home'
-import GigList from '../pages/GigList/GigList'
-import Login from '../pages/Login/Login'
-import Register from '../pages/Register/Register'
+import Home from '../pages/Home/Home';
+import GigList from '../pages/GigList/GigList';
+import Login from '../pages/Login/Login';
+import Register from '../pages/Register/Register';
+import GigAdder from '../pages/GigAdder/GigAdder';
 
 const Routes = (props) => (
-    <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/gigs" component={GigList} />
-        <Route exact path="/login" render={() => <Login setCurrentUserEmail={props.setCurrentUserEmail} setPassword={props.setPassword} handleLogin={props.handleLogin}/>} />
-        <Route exact path="/register" render={() => <Register handleRegister={props.handleRegister} setCurrentUserEmail={props.setCurrentUserEmail} setPassword={props.setPassword} setUsername={props.setUsername}/> } />
-    </Switch>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/gigs" component={GigList} />
+    <Route exact path="/login" render={() => <Login setCurrentUserEmail={props.setCurrentUserEmail} setPassword={props.setPassword} handleLogin={props.handleLogin} />} />
+    <Route exact path="/register" render={() => <Register handleRegister={props.handleRegister} setCurrentUserEmail={props.setCurrentUserEmail} setPassword={props.setPassword} setUsername={props.setUsername} />} />
+    <Route exact path="/newgig" component={GigAdder} />
+  </Switch>
 );
 
 export default Routes;
