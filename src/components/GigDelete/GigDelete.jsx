1cf2227a9a2a7d1c585b91d99497e6bdf42020a3
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-20 17:34:27 -0600

    gig delete button works, need to redirect page tho

--- src/components/GigDelete/GigDelete.jsx
+++ src/components/GigDelete/GigDelete.jsx
@@ -1,6 +1,22 @@
-const DeletePost =  ( gigs ) => {
+import axios from 'axios';
 
-    console.log('hi')
-}
+const GigDelete = ({ gig }) => {
+  // delete method
+  const deletePost = async () => {
+    try {
+      console.log('GigDelete()', gig);
+      const response = await axios.delete(`http://localhost:5000/api/gigs/${gig._id}`);
+      console.log(response);
+    } catch (error) {
+      console.log(error, 'ERROR IN <GigDelete>.deletePost', gig);
+    }
+  };
 
-export default DeletePost;
+  return (
+    <button type="submit" className="btn btn-outline-danger" onClick={deletePost}>
+      Delete
+    </button>
+  );
+};
+
+export default GigDelete;
