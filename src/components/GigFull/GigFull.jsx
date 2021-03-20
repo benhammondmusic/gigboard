0b5824fbd757359952133ef4df402314f29a93b7
Hayden Harris <haydenharris0@gmail.com>
2021-03-20 11:10:16 -0600

    added edit & delete buttons

--- src/components/GigFull/GigFull.jsx
+++ src/components/GigFull/GigFull.jsx
@@ -19,6 +19,17 @@ const GigFull = ({ gig }) => (
           </li>
         );
       })}
+
+      {/* Button to edit the post --need to make it functional & only accesible to the user who made it  */}
+      <form action="/articles/<%= article.id %>?_method=DELETE" method="POST" class="d-inline align-right">
+        <button type="submit" class="btn btn-outline-dark">Edit</button>
+      </form>
+      
+      {/* Button to delete the post --need to make it functional & only accesible to the user who made it  */}
+      <form action="/articles/<%= article.id %>?_method=DELETE" method="POST" class="d-inline align-right">
+        <button type="submit" class="btn btn-outline-danger">Delete</button>
+      </form>
+
     </ul>
   </>
 );
