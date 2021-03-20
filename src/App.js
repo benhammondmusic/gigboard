214530e9848312d759c5f03fcc26f36da1732a06
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-20 12:11:42 -0600

    register is working, login is not

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 // import React from 'react'
 
-import "./App.scss";
+import './App.scss';
 // import {Button} from 'react-bootstrap/Button'
-import Auth from "./Models/Auth";
-import Routes from "./config/routes";
-import Navbar from "./components/Navbar/Navbar";
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
+import Auth from './Models/Auth';
+import Routes from './config/routes';
+import Navbar from './components/Navbar/Navbar';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 // DEVELOPMENT API: localhost:5000 for heroku local web
 // PRODUCTION API: use a deployed backend on heroku
 
 function App() {
-  const [formPassword, setFormPassword] = useState("");
-  const [formEmail, setFormEmail] = useState("");
-  const [currentUserEmail, setCurrentUserEmail] = useState(localStorage.getItem("uid"));
+  const [formPassword, setFormPassword] = useState('');
+  const [formEmail, setFormEmail] = useState('');
+  const [currentUserEmail, setCurrentUserEmail] = useState(localStorage.getItem('uid'));
   console.log({ currentUserEmail });
 
   const history = useHistory();
@@ -23,15 +23,15 @@ function App() {
     e.preventDefault();
     console.log(e);
     try {
-      console.log(formEmail);
-      const user = await Auth.register({ formEmail, formPassword });
-      console.log(user, "user created");
+      console.log(formEmail, 'formEmail inside handleReg');
+      const user = await Auth.register({ email: formEmail, password: formPassword });
+      console.log(user, 'user created');
       const token = user.data.signedJwt;
 
-      localStorage.setItem("uid", token);
+      localStorage.setItem('uid', token);
       setCurrentUserEmail(token);
 
-      history.push("/gigs");
+      history.push('/gigs');
     } catch (error) {
       console.log(error);
     }
@@ -40,14 +40,16 @@ function App() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const user = await Auth.login({ formEmail, formPassword });
+      console.log(formEmail, 'formEmail inside handleLogin');
+      const user = await Auth.login({ email: formEmail, password: formPassword });
+      console.log(user, 'user after Auth.login() inside handleLogin');
       const token = user.data.signedJwt;
 
-      localStorage.setItem("uid", token);
+      localStorage.setItem('uid', token);
 
       setCurrentUserEmail(token);
 
-      history.push("/gigs");
+      history.push('/gigs');
     } catch (error) {
       console.log(error);
     }
@@ -55,23 +57,15 @@ function App() {
 
   const logOut = () => {
     localStorage.clear();
-    setCurrentUserEmail(localStorage.getItem("uid"));
-    history.push("/");
+    setCurrentUserEmail(localStorage.getItem('uid'));
+    history.push('/');
   };
 
   return (
     <>
       <Navbar logOut={logOut} currentUserEmail={currentUserEmail} />
       <main>
-        <Routes
-          currentUserEmail={currentUserEmail}
-          setCurrentUserEmail={setCurrentUserEmail}
-          handleLogin={handleLogin}
-          handleRegister={handleRegister}
-          formPassword={formPassword}
-          setFormPassword={setFormPassword}
-          setFormEmail={setFormEmail}
-        />
+        <Routes currentUserEmail={currentUserEmail} setCurrentUserEmail={setCurrentUserEmail} handleLogin={handleLogin} handleRegister={handleRegister} formPassword={formPassword} setFormPassword={setFormPassword} setFormEmail={setFormEmail} />
       </main>
     </>
   );
--- src/Models/Auth.js
+++ src/Models/Auth.js
@@ -1,10 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 class Auth {
   static register = (userData) => {
+    console.log(userData, 'userData in Auth.register');
     return axios.post(`${process.env.REACT_APP_API_URL}/users/register`, userData);
   };
   static login = (userData) => {
+    console.log(userData, 'userData in Auth.login');
     return axios.post(`${process.env.REACT_APP_API_URL}/users/login`, userData);
   };
 }
