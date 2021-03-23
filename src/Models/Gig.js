99a2c73141863e51317b5807d6bb8b90b5c121d2
JC Coles <jcoles1155@gmail.com>
2021-03-22 18:36:28 -0700

    merge conflicts

--- src/Models/Gig.js
+++ src/Models/Gig.js
@@ -13,28 +13,13 @@ class Gig {
     return axios.put(`http://localhost:5000/api/gigs/${gigId}`, gigFormData);
   };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    static update = ( gigId, gigFormData ) => {
-        return axios.put(`http://localhost:5000/api/gigs/${gigId}`, gigFormData)
-    } 
-=======
-  static destroy = async (gigId) => {
-    console.log('DELETING GIG: ', gigId);
-
-=======
   static destroy = (gigId) => {
->>>>>>> 710ca680ec19b6d45dd7ccab1c03da46c0a1e911
     try {
       return axios.delete(`http://localhost:5000/api/gigs/${gigId}`);
     } catch (error) {
       console.log(error, 'ERROR DELETING GIG');
     }
   };
-<<<<<<< HEAD
->>>>>>> 52f1bcd4f394a213d09be7ae84683c7dcba5019a
-=======
->>>>>>> 710ca680ec19b6d45dd7ccab1c03da46c0a1e911
 }
 
 export default Gig;
