771ef2b9ee8b67b4e3c7795ec8a8f4317da09df1
Kaye Roberts <pkaye@Kayes-MacBook-Pro.local>
2021-03-24 18:53:15 -0700

    Fixed register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 // import { useHistory } from 'react-router-dom';
 import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from 'react-router-dom'
 
 import './Register.css'
 
@@ -11,8 +12,10 @@ import './Register.css'
 const Register = (props) => {
   return (
     <>
-      <h1>Register A New Gig Poster</h1>
+     
       <Form onSubmit={props.handleRegister} className="registerForm">
+      <h4>Register A New Gig Poster</h4>
+      <hr />
         <Form.Group controlId="formBasicEmail">
           <Form.Label className="form-title">Email address</Form.Label>
           <Form.Control required type="email" placeholder="Enter email" onChange={(e) => props.setFormEmail(e.target.value)} />
@@ -26,6 +29,9 @@ const Register = (props) => {
         <Button variant="primary" type="submit" id="login-btn">
           Register
         </Button>
+        <Link to="/login" className="btn btn-secondary reg-cancel">
+          Cancel
+        </Link>
       </Form>
     </>
   );
