d2623b94e513288914052240aeb9c579b93581a8
Sergio Paiva Luna <sepaivaluna@sergios-mbp.home>
2021-03-26 00:14:44 -0400

    added redirect to login after registering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.scss';
-import Auth from './Models/Auth';
-import Routes from './config/routes';
-import Navbar from './components/Navbar/Navbar';
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useEffect } from 'react';
+import "./App.scss";
+import Auth from "./Models/Auth";
+import Routes from "./config/routes";
+import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
 
 // DEVELOPMENT API: localhost:5000 for heroku local web
 // PRODUCTION API: use a deployed backend on heroku
 
 function App() {
-  const [formPassword, setFormPassword] = useState('');
-  const [formEmail, setFormEmail] = useState('');
-  const [currentUserEmail, setCurrentUserEmail] = useState('');
-  const [currentUserId, setCurrentUserId] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [formPassword, setFormPassword] = useState("");
+  const [formEmail, setFormEmail] = useState("");
+  const [currentUserEmail, setCurrentUserEmail] = useState("");
+  const [currentUserId, setCurrentUserId] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
-    setCurrentUserEmail(localStorage.getItem('currentUserEmail') || '');
-    setCurrentUserId(localStorage.getItem('currentUserId') || '');
+    setCurrentUserEmail(localStorage.getItem("currentUserEmail") || "");
+    setCurrentUserId(localStorage.getItem("currentUserId") || "");
   }, []);
 
   const history = useHistory();
@@ -31,9 +31,9 @@ function App() {
       setCurrentUserId(res.data.currentUserId);
       setCurrentUserEmail(JSON.parse(res.config.data).email);
 
-      history.push('/gigs');
+      history.push("/login");
     } catch (error) {
-      console.log(error, 'error');
+      console.log(error, "error");
       // console.log(error.message, 'error . message');
       setErrorMessage(error);
     }
@@ -48,9 +48,9 @@ function App() {
 
       setCurrentUserEmail(JSON.parse(res.config.data).email);
 
-      localStorage.setItem('jwt', res.data.token);
+      localStorage.setItem("jwt", res.data.token);
 
-      history.push('/gigs');
+      history.push("/gigs");
     } catch (error) {
       console.log(error);
       setErrorMessage(error);
@@ -60,23 +60,23 @@ function App() {
   // When user info changes, the info will be set in local storage
   // If user is logged out, nothing is in storage cd: 74-80.
   useEffect(() => {
-    if (currentUserId) localStorage.setItem('currentUserId', currentUserId);
+    if (currentUserId) localStorage.setItem("currentUserId", currentUserId);
   }, [currentUserId]);
 
   useEffect(() => {
-    if (currentUserEmail) localStorage.setItem('currentUserEmail', currentUserEmail);
+    if (currentUserEmail) localStorage.setItem("currentUserEmail", currentUserEmail);
   }, [currentUserEmail]);
 
   const logOut = () => {
     setCurrentUserEmail();
     setCurrentUserId();
     localStorage.clear();
-    history.push('/');
+    history.push("/");
   };
 
   const clearErrors = () => {
-    console.log('clearing any errors messages from App.js state');
-    setErrorMessage('');
+    console.log("clearing any errors messages from App.js state");
+    setErrorMessage("");
   };
 
   useEffect(() => {
@@ -87,7 +87,19 @@ function App() {
     <>
       <Navbar logOut={logOut} currentUserEmail={currentUserEmail} />
       <main>
-        <Routes clearErrors={clearErrors} errorMessage={errorMessage} currentUserId={currentUserId} setCurrentUserId={setCurrentUserId} currentUserEmail={currentUserEmail} setCurrentUserEmail={setCurrentUserEmail} handleLogin={handleLogin} handleRegister={handleRegister} formPassword={formPassword} setFormPassword={setFormPassword} setFormEmail={setFormEmail} />
+        <Routes
+          clearErrors={clearErrors}
+          errorMessage={errorMessage}
+          currentUserId={currentUserId}
+          setCurrentUserId={setCurrentUserId}
+          currentUserEmail={currentUserEmail}
+          setCurrentUserEmail={setCurrentUserEmail}
+          handleLogin={handleLogin}
+          handleRegister={handleRegister}
+          formPassword={formPassword}
+          setFormPassword={setFormPassword}
+          setFormEmail={setFormEmail}
+        />
       </main>
     </>
   );
