467f5fb07f927a4ba22123638f493befcde105cc
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-26 16:12:46 -0600

    adds date logic to <GigAdd>

--- src/pages/GigAdder/GigAdder.jsx
+++ src/pages/GigAdder/GigAdder.jsx
@@ -33,6 +33,7 @@ const GigAdder = ({ currentUserEmail, currentUserId }) => {
   // const [expirationDate, setExpirationDate] = useState('');
   const [workStartDate, setWorkStartDate] = useState('');
   const [workEndDate, setWorkEndDate] = useState('');
+  const [dateError, setDateError] = useState('');
   const history = useHistory();
 
   // NOTE checking to see if user is logged in
@@ -41,6 +42,19 @@ const GigAdder = ({ currentUserEmail, currentUserId }) => {
       history.push('/gigs');
     }
   });
+
+  // make sure START DATE is always before END DATE
+  useEffect(() => {
+    console.log('useEffect() when workstart changes. START:', workStartDate, 'END:', workEndDate);
+    // every time dates change, set error to false
+    // setDateError('');
+    // console.log('date error in useeffect:', dateError);
+    if (workEndDate && workEndDate < workStartDate) {
+      // when the dates are wrong, fix them and set error to true
+      setWorkEndDate(workStartDate);
+      setDateError('End date cannot be earlier than Start date.');
+    }
+  }, [workStartDate, workEndDate]);
 
   // GIG POSTER CLICKS "POST GIG" BUTTON
   const handleSubmit = async (e) => {
@@ -73,11 +87,9 @@ const GigAdder = ({ currentUserEmail, currentUserId }) => {
           <Form.Label className="form-title">What's the Gig?</Form.Label>
           <Form.Control value={title} type="title" placeholder="Gig goes here" onChange={(e) => setTitle(e.target.value)} />
         </Form.Group>
-
         <Form.Group controlId="input2">
           <Form.Control as="textarea" rows={3} placeholder="Describe your available Gig (include how many hours)" onChange={(e) => setDescription(e.target.value)} />
         </Form.Group>
-
         <Form.Group controlId="input3">
           <Form.Label className="form-title">What does the Gig pay?</Form.Label>
           <div className="pay-form-container">
@@ -85,7 +97,6 @@ const GigAdder = ({ currentUserEmail, currentUserId }) => {
             <Form.Control type="textarea" placeholder="Pay/day goes here" onChange={(e) => setPay(e.target.value)} />
           </div>
         </Form.Group>
-
         <Form.Group controlId="formBasicCheckbox">
           <Form.Check
             type="checkbox"
@@ -99,12 +110,10 @@ const GigAdder = ({ currentUserEmail, currentUserId }) => {
             }}
           />
         </Form.Group>
-
         <Form.Group controlId="input3">
           <Form.Label className="form-title">Where's the Gig?</Form.Label>
           <Form.Control type="location" placeholder="Location" onChange={(e) => setLocation(e.target.value)} />
         </Form.Group>
-
         <Form.Group controlId="urgencySelect">
           <Form.Label className="form-title">Urgency?</Form.Label>
           <Form.Control as="select" type="urgency" onChange={(e) => setUrgency(e.target.value)}>
@@ -113,7 +122,6 @@ const GigAdder = ({ currentUserEmail, currentUserId }) => {
             <option value="High">High</option>
           </Form.Control>
         </Form.Group>
-
         <Form.Group controlId="tags">
           <Form.Label className="form-title">What category tag(s) does this Gig fit?</Form.Label>
           <Multiselect
@@ -124,24 +132,35 @@ const GigAdder = ({ currentUserEmail, currentUserId }) => {
             }}
           />
         </Form.Group>
-
         <Form.Group controlId="workStartDate">
           <Form.Label className="form-title">When does this Gig start?</Form.Label> &nbsp;
-          <DatePicker selected={workStartDate} onChange={(date) => setWorkStartDate(date)} />
+          <DatePicker
+            selected={workStartDate}
+            onClick={(e) => setDateError('')}
+            onChange={(date) => {
+              setWorkStartDate(date);
+              setDateError('');
+            }}
+          />
         </Form.Group>
-
         <Form.Group controlId="workEndDate">
           <Form.Label className="form-title">When does this Gig end?</Form.Label> &nbsp;
-          <DatePicker selected={workEndDate} onChange={(date) => setWorkEndDate(date)} />
+          <DatePicker
+            selected={workEndDate}
+            onChange={(date) => {
+              setWorkEndDate(date);
+              setDateError('');
+            }}
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit" onClick={(event) => (window.location.href = '/gigs')}>
           Post Gig
         </Button>
-
         <Link to="/gigs" className="btn btn-secondary cncl-btn">
           Cancel
         </Link>
+        <span className="error">{dateError}</span>
       </Form>
     </div>
   );
