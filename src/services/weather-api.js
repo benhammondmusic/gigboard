ae05ab964ca11cb294c874d1f2c7724dc53905fa
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-27 08:02:26 -0600

    run weather over https

--- src/services/weather-api.js
+++ src/services/weather-api.js
@@ -1,7 +1,7 @@
 async function weatherByLocation(location) {
-  let endpoint = `http://api.weatherapi.com/v1/current.json?key=627d050e37f6439592435238212603&q=${location}&aqi=no`;
+  let endpoint = `https://api.weatherapi.com/v1/current.json?key=627d050e37f6439592435238212603&q=${location}&aqi=no`;
 
-  return await fetch(endpoint, { mode: "cors" }).then((res) => res.json());
+  return await fetch(endpoint, { mode: 'cors' }).then((res) => res.json());
 }
 
 export { weatherByLocation };
