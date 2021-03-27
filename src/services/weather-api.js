08ccebb75ef28aba7fdd4ed274494844def6bcd7
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-27 08:17:30 -0600

    https

--- src/services/weather-api.js
+++ src/services/weather-api.js
@@ -1,5 +1,9 @@
 async function weatherByLocation(location) {
-  let endpoint = `https://api.weatherapi.com/v1/current.json?key=627d050e37f6439592435238212603&q=${location}&aqi=no`;
+  const { REACT_APP_WEATHER_API_KEY } = process.env;
+
+  // console.log(REACT_APP_WEATHER_API_KEY);
+
+  let endpoint = `https://api.weatherapi.com/v1/current.json?key=${REACT_APP_WEATHER_API_KEY}&q=${location}&aqi=no`;
 
   return await fetch(endpoint, { mode: 'cors' }).then((res) => res.json());
 }
