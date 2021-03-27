d3232261cd49afe12a7f8495c00f4aebf5c9bdf0
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-27 08:59:53 -0600

    move weather key to env

--- src/services/weather-api.js
+++ src/services/weather-api.js
@@ -1,7 +1,11 @@
 async function weatherByLocation(location) {
-  let endpoint = `http://api.weatherapi.com/v1/current.json?key=627d050e37f6439592435238212603&q=${location}&aqi=no`;
+  const { REACT_APP_WEATHER_API_KEY } = process.env;
 
-  return await fetch(endpoint, { mode: "cors" }).then((res) => res.json());
+  // console.log(REACT_APP_WEATHER_API_KEY);
+
+  let endpoint = `https://api.weatherapi.com/v1/current.json?key=${REACT_APP_WEATHER_API_KEY}&q=${location}&aqi=no`;
+
+  return await fetch(endpoint, { mode: 'cors' }).then((res) => res.json());
 }
 
 export { weatherByLocation };
